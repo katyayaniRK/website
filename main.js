cf27c896043d415f3ace8cca9d2d3b54aa1c42de
katyayaniRK <katyayani.g24@gmail.com>
2024-09-06 17:02:03 +0430

    website deployment

---
+++ main.js
@@ -0,0 +1,20 @@
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
+})
+ 
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq=>{
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open');
+
+        //icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className==='fa-solid fa-plus'){
+            icon.className='fa-solid fa-minus';
+        }else{
+            icon.className='fa-solid fa-plus';
+
+        }
+    })
+})
